83590c1730ce20919d9615a06b38ea92ea72ceda
lnikon <vagbej@gmail.com>
2018-05-19 23:09:56 +0400

    vector with sum function and approp test added

--- test/test.cpp
+++ test/test.cpp
@@ -1,4 +1,5 @@
 #include <hello_world.hpp>
+#include <vector.hpp>
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 
@@ -18,4 +19,11 @@ TEST_CASE("test_expensive")
     std::size_t work = 100;
     std::string result = hello_world::expensive(work);
     REQUIRE(result == std::string("Expensive work is finished"));
+}
+
+TEST_CASE("test_sum")
+{
+    implstl::vector<int> vec;
+    int result = vec.sum(2, 2);
+    REQUIRE(result == 4);
 }
